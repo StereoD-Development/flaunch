747bd1230cab3f4cb7fb0da31c5fd992bf431ffc
Michael McCartney <mmccartney@stereodllc.com>
2019-05-31 12:04:42 -0400

    Fill out basic commands - Added the WRITE command

--- src/build/commands/basic.py
+++ src/build/commands/basic.py
@@ -29,15 +29,43 @@ class CopyCommand(_BuildCommand):
         """
         Copying files has many-a-caveat
         """
+        parser.add_argument('-m', '--make-dirs', action='store_true', help='Create the destination directory')
+        parser.add_argument('-f', '--force', action='store_true', help='Overwrite any files that already exist')
         parser.add_argument('source', help='The source location')
         parser.add_argument('destination', help='The destination location')
-
 
     def run(self, build_file):
         """
         Copy files in a platform agnostic way
         """
-        pass # TODO
+        if self.data.make_dirs:
+            if not self.data.force and os.path.exists(self.data.destination):
+                raise RuntimeError(
+                    'Destination Already Exists: {} (Consider using the --force flag)' \
+                    .format(self.data.destination)
+                )
+
+            elif not os.path.exists(os.path.dirname(self.data.destination)):
+                os.makedirs(os.path.dirname(self.data.destination))
+
+
+        elif not os.path.isdir(os.path.dirname(self.data.destination)):
+            raise RuntimeError(
+                'Destination directory does not exist! {} (Consider using the --make-dirs flag)' \
+                .format(os.path.dirname(self.data.destination))
+            )
+
+        if not self.data.force and os.path.exists(self.data.destination):
+            raise RuntimeError(
+                'Destination Already Exists: {} (Consider using the --force flag)' \
+                .format(self.data.destination)
+            )
+
+        logging.info('Copying: {} -> {}'.format(self.data.source, self.data.destination))
+        if os.path.isdir(self.data.source):
+            shutil.copytree(self.data.source, self.data.destination)
+        else:
+            shutil.copy2(self.data.source, self.data.destination)
 
 
 class MoveCommand(_BuildCommand):
@@ -55,7 +83,6 @@ class MoveCommand(_BuildCommand):
         """
         Moving files takes at least a source and a destination
         """
-        # TODO: Add arguments
         parser.add_argument('-m', '--make-dirs', action='store_true', help='Create the destination directory')
         parser.add_argument('-f', '--force', action='store_true', help='Overwrite any files that already exist')
         parser.add_argument('source', help="The source file(s)")
@@ -74,7 +101,7 @@ class MoveCommand(_BuildCommand):
                     .format(self.data.destination)
                 )
 
-            else:
+            elif not os.path.exists(os.path.dirname(self.data.destination)):
                 os.makedirs(os.path.dirname(self.data.destination))
 
 
@@ -84,6 +111,14 @@ class MoveCommand(_BuildCommand):
                 .format(os.path.dirname(self.data.destination))
             )
 
+        if not self.data.force and os.path.exists(self.data.destination):
+            raise RuntimeError(
+                'Destination Already Exists: {} (Consider using the --force flag)' \
+                .format(self.data.destination)
+            )
+
+        logging.info('Moving: {} -> {}'.format(self.data.source, self.data.destination))
+        shutil.move(self.data.source, self.data.destination)
 
 
 class PrintCommand(_BuildCommand):
@@ -132,3 +167,52 @@ class PrintCommand(_BuildCommand):
             return t
 
         print (' '.join(map(_clean, self.data.texts)))
+
+
+class WriteCommand(_BuildCommand):
+    """
+    Write text to a file
+    """
+    alias = 'WRITE'
+
+    def description(self):
+        return 'Write data to a file'
+
+
+    def populate_parser(self, parser):
+        """
+        We need the text to commit as well as the file we're outputing
+        it to.
+        """
+        parser.add_argument(
+            '-a', '--append',
+            action='store_true',
+            help='Should we just append to the file. (Defaults to overwrite)'
+        )
+        parser.add_argument(
+            'content',
+            help='The content to store within out file'
+        )
+        parser.add_argument(
+            'file',
+            help='The to write our information to'
+        )
+
+
+    def run(self, build_file):
+        """
+        Run the write command (but not the wrong one bum-dum-chhh)
+        """
+        open_type = 'a' if self.data.append else 'w'
+        if not os.path.isfile(self.data.file):
+            open_type = 'w' # Nothing to append to
+
+        logging.info('Writing to file: {}'.format(self.data.file))
+
+        try:
+            with open(self.data.file, open_type) as f:
+                f.write(self.data.content)
+        except OSError as e:
+            raise RuntimeError("Could not open file: {} - {}".format(
+                self.data.file, str(e)
+            ))
